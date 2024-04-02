e5d5c8ddf349d7fdc829b8be027e5ae7ded0ab67
Wendell <wendellwangxinyu@icloud.com>
2024-04-02 13:15:21 +0800

    Format Package.swift

--- Package.swift
+++ Package.swift
@@ -1,36 +1,36 @@
 // swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-import PackageDescription
 import CompilerPluginSupport
+import PackageDescription
 
 let package = Package(
-    name: "CodableKit",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
-    products: [
-        .library(
-            name: "CodableKit",
-            targets: ["CodableKit"]
-        ),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
-    ],
-    targets: [
-        .macro(
-            name: "CodableKitMacros",
-            dependencies: [
-                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
-                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
-            ]
-        ),
-        .target(name: "CodableKit", dependencies: ["CodableKitMacros"]),
-        .testTarget(
-            name: "CodableKitTests",
-            dependencies: [
-                "CodableKitMacros",
-                .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
-            ]
-        ),
-    ]
+  name: "CodableKit",
+  platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+  products: [
+    .library(
+      name: "CodableKit",
+      targets: ["CodableKit"]
+    )
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-syntax.git", from: "510.0.0")
+  ],
+  targets: [
+    .macro(
+      name: "CodableKitMacros",
+      dependencies: [
+        .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+        .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+      ]
+    ),
+    .target(name: "CodableKit", dependencies: ["CodableKitMacros"]),
+    .testTarget(
+      name: "CodableKitTests",
+      dependencies: [
+        "CodableKitMacros",
+        .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
+      ]
+    ),
+  ]
 )
