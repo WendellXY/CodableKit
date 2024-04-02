7e1bf6224597dab94c6a524c49394bbf0d1b4ca3
Wendell <wendellwangxinyu@icloud.com>
2024-04-02 16:58:53 +0800

    Update test cases

--- Tests/CodableKitTests/CodableKitTests.swift
+++ Tests/CodableKitTests/CodableKitTests.swift
@@ -14,13 +14,110 @@ import XCTest
 #if canImport(CodableKitMacros)
 import CodableKitMacros
 
-let testMacros: [String: Macro.Type] = [
-  "Codable": CodableMacro.self
+let macros: [String: any Macro.Type] = [
+  "Codable": CodableMacro.self,
+  "CodableKey": CodableKeyMacro.self,
 ]
 #endif
 
 final class CodableKitTests: XCTestCase {
-  func testMacro() throws {
+  func testMacros() throws {
+    #if canImport(CodableKitMacros)
+    assertMacroExpansion(
+      """
+      @Codable
+      public struct User {
+        let id: UUID
+        let name: String
+        let age: Int
+      }
+      """,
+      expandedSource: """
+        public struct User {
+          let id: UUID
+          let name: String
+          let age: Int
+        }
+
+        extension User: Codable {
+          enum CodingKeys: String, CodingKey {
+            case id
+            case name
+            case age
+          }
+
+          public init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            id = try container.decode(UUID.self, forKey: .id)
+            name = try container.decode(String.self, forKey: .name)
+            age = try container.decode(Int.self, forKey: .age)
+          }
+
+          public func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            try container.encode(id, forKey: .id)
+            try container.encode(name, forKey: .name)
+            try container.encode(age, forKey: .age)
+          }
+        }
+        """,
+      macros: macros,
+      indentationWidth: .spaces(2)
+    )
+    #else
+    throw XCTSkip("macros are only supported when running tests for the host platform")
+    #endif
+  }
+
+  func testMacroWithDefaultValue() throws {
+    #if canImport(CodableKitMacros)
+    assertMacroExpansion(
+      """
+      @Codable
+      public struct User {
+        let id: UUID
+        let name: String
+        var age: Int = 24
+      }
+      """,
+      expandedSource: """
+        public struct User {
+          let id: UUID
+          let name: String
+          var age: Int = 24
+        }
+
+        extension User: Codable {
+          enum CodingKeys: String, CodingKey {
+            case id
+            case name
+            case age
+          }
+
+          public init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            id = try container.decode(UUID.self, forKey: .id)
+            name = try container.decode(String.self, forKey: .name)
+            age = try container.decodeIfPresent(Int.self, forKey: .age) ?? 24
+          }
+
+          public func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            try container.encode(id, forKey: .id)
+            try container.encode(name, forKey: .name)
+            try container.encode(age, forKey: .age)
+          }
+        }
+        """,
+      macros: macros,
+      indentationWidth: .spaces(2)
+    )
+    #else
+    throw XCTSkip("macros are only supported when running tests for the host platform")
+    #endif
+  }
+
+  func testMacroWithCodableKeyAndDefaultValue() throws {
     #if canImport(CodableKitMacros)
     assertMacroExpansion(
       """
@@ -36,23 +133,24 @@ final class CodableKitTests: XCTestCase {
         public struct User {
           let id: UUID
           let name: String
-          @CodableKey("currentAge")
           var age: Int = 24
         }
 
-        public extension User: Codable {
+        extension User: Codable {
           enum CodingKeys: String, CodingKey {
             case id
             case name
             case age = "currentAge"
           }
-          init(from decoder: Decoder) throws {
+
+          public init(from decoder: Decoder) throws {
             let container = try decoder.container(keyedBy: CodingKeys.self)
             id = try container.decode(UUID.self, forKey: .id)
             name = try container.decode(String.self, forKey: .name)
             age = try container.decodeIfPresent(Int.self, forKey: .age) ?? 24
           }
-          func encode(to encoder: Encoder) throws {
+
+          public func encode(to encoder: Encoder) throws {
             var container = encoder.container(keyedBy: CodingKeys.self)
             try container.encode(id, forKey: .id)
             try container.encode(name, forKey: .name)
@@ -60,7 +158,55 @@ final class CodableKitTests: XCTestCase {
           }
         }
         """,
-      macros: testMacros,
+      macros: macros,
+      indentationWidth: .spaces(2)
+    )
+    #else
+    throw XCTSkip("macros are only supported when running tests for the host platform")
+    #endif
+  }
+
+  func testMacroWithOptionalValue() throws {
+    #if canImport(CodableKitMacros)
+    assertMacroExpansion(
+      """
+      @Codable
+      public struct User {
+        let id: UUID
+        let name: String
+        var age: Int? = 24
+      }
+      """,
+      expandedSource: """
+        public struct User {
+          let id: UUID
+          let name: String
+          var age: Int? = 24
+        }
+
+        extension User: Codable {
+          enum CodingKeys: String, CodingKey {
+            case id
+            case name
+            case age
+          }
+
+          public init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            id = try container.decode(UUID.self, forKey: .id)
+            name = try container.decode(String.self, forKey: .name)
+            age = try container.decodeIfPresent(Int?.self, forKey: .age) ?? 24
+          }
+
+          public func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            try container.encode(id, forKey: .id)
+            try container.encode(name, forKey: .name)
+            try container.encode(age, forKey: .age)
+          }
+        }
+        """,
+      macros: macros,
       indentationWidth: .spaces(2)
     )
     #else
