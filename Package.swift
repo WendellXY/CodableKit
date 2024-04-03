505a10fc6053b2aa9633337d079ce10599032cb7
Wendell <wendellwangxinyu@icloud.com>
2024-04-03 14:07:41 +0800

    Add dooc plugin

--- Package.swift
+++ Package.swift
@@ -14,7 +14,8 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-syntax.git", from: "510.0.0")
+    .package(url: "https://github.com/apple/swift-syntax.git", from: "510.0.0"),
+    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0"),
   ],
   targets: [
     .macro(
