4b8cedf6590a9e9e25e919c478f64ffaa2a35e96
Wendell <wendellwangxinyu@icloud.com>
2024-06-12 15:48:07 +0800

    Remove docc plugin

--- Package.swift
+++ Package.swift
@@ -16,7 +16,6 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-syntax.git", from: "510.0.0"),
-    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0"),
   ],
   targets: [
     .target(
